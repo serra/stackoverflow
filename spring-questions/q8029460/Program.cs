0dafe9eb2c6a7469abf2e8360a00cbd49267e727
Marijn van der Zee <marijn@serraict.com>
2011-11-06 22:39:46 +0100

    For http://stackoverflow.com/questions/8029460/spring-net-logging-example

---
+++ spring-questions/q8029460/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+using NUnit.Framework;
+using Spring.Aop;
+using Spring.Context.Support;
+
+namespace q8029460
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+
+    [TestFixture]
+    public class q8029460Tests
+    {
+        [Test]
+        public void Main()
+        {
+            var ctx = ContextRegistry.GetContext();
+            var ohI = (MyClass)ctx["mySomeClass"];
+            ohI.WantToLogThisCall();
+            Console.WriteLine(ohI);
+        }
+    }
+
+    public class MyClass
+    {
+        [LogCall]
+        public virtual void WantToLogThisCall()
+        {
+            Console.WriteLine("Should have been logged before.");
+        }
+    }
+
+    public class LogCallInterceptor : IMethodBeforeAdvice
+    {
+        public void Before(MethodInfo method, object[] args, object target)
+        {
+            Console.WriteLine("Before calling method: {0}", method.Name);
+        }
+    }
+
+    [Serializable]
+    [AttributeUsage(AttributeTargets.Method)]
+    public class LogCallAttribute : Attribute
+    {
+    }
+
+}
