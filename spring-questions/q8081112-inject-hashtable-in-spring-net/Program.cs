07c6c5ca63ce05df6c9361ae4bf6d4fdcf0897f2
Marijn van der Zee <marijn@serraict.com>
2011-11-14 16:58:59 +0100

    Add two projects.

---
+++ spring-questions/q8081112-inject-hashtable-in-spring-net/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Spring.Context.Support;
+
+namespace q8081112_inject_hashtable_in_spring_net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+
+    [TestFixture]
+    public class DictionaryTests
+    {
+        [Test]
+        public void Main()
+        {
+            var ctx = new XmlApplicationContext("objects.xml");
+            var o = (MyClass)ctx.GetObject("MyObject");
+
+            Assert.IsNotNull(o.MyHashTable);
+
+            Assert.AreEqual("value1", o.MyHashTable["key1"]);
+
+            Console.WriteLine(o);
+        }
+    }
+
+    public class MyClass
+    {
+        public Hashtable MyHashTable { get; set; }
+    }
+
+}
---
+++ spring-questions/q8121697/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Xml.Serialization;
+using NUnit.Framework;
+using Spring.Aop;
+using Spring.Aop.Framework;
+using Spring.Context.Support;
+
+namespace q8121697
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myObject = new MyClass();
+
+
+            // this creates a dynamic assembly 'Spring.Proxy'
+            var factory = new ProxyFactory(myObject);
+            factory.AddAdvice(new LogCallInterceptor());
+            var myProxy = (IMyInterface)factory.GetProxy();
+            myProxy.DoSomething();
+            Console.WriteLine(Assembly.GetAssembly(myProxy.GetType()).FullName);
+
+            Console.WriteLine();
+
+            // this creates a dynamic assembly 'Spring.Proxy'
+            var myObject2 = new MyClass2();
+            var factory2 = new ProxyFactory(myObject2);
+            factory2.AddAdvice(new LogCallInterceptor());
+            factory2.ProxyTargetType = true;
+            var myProxy2 = (MyClass2)factory2.GetProxy();
+            myProxy2.DoSomething();
+            Console.WriteLine(Assembly.GetAssembly(myProxy2.GetType()).FullName);
+
+            Console.WriteLine();
+
+            // this creates a dynamic assembly for the xml serializer (the strnge assembly name from question?)
+            var s = new XmlSerializer(typeof(MyClass));
+            s.Serialize(Console.Out, myObject);
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+
+    
+
+    [TestFixture]
+    public class DictionaryTests
+    {
+        [Test]
+        public void Main()
+        {
+            var ctx = new XmlApplicationContext("objects.xml");
+            var o = ctx.GetObject("MyObject");
+            Console.WriteLine(o);
+        }
+    }
+
+    public interface IMyInterface
+    {
+        void DoSomething();
+    }
+
+    public class MyClass : IMyInterface
+    {
+        public string Name { get; set; }
+
+        public virtual void DoSomething()
+        {
+            Console.WriteLine("Doing something ... Done.");
+        }
+    }
+
+    public class MyClass2 
+    {
+        public virtual string Name { get; set; }
+
+        public virtual void DoSomething()
+        {
+            Console.WriteLine("Doing something ... Done.");
+        }
+    }
+
+    public class LogCallInterceptor : IMethodBeforeAdvice
+    {
+        public void Before(MethodInfo method, object[] args, object target)
+        {
+            Console.WriteLine("Before calling method: {0}", method.Name);
+        }
+    }
+
+}
