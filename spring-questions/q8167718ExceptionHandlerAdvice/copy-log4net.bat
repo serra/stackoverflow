c27cd25f1b716ca332ebc9a4c7d2f469404151d7
Marijn van der Zee <marijn@serraict.com>
2011-12-09 10:41:13 +0100

    Add post build event that copies log4net to output directory

---
+++ spring-questions/q8167718ExceptionHandlerAdvice/copy-log4net.bat
@@ -0,0 +1,5 @@
+﻿:: %1 solution dir
+:: %2 targetdir
+copy %1\packages\Common.Logging.2.0.0\lib\2.0\*.* %2
+copy %1\packages\Common.Logging.Log4Net.2.0.0\lib\2.0\*.* %2
+copy %1\packages\log4net.1.2.10\lib\2.0\*.* %2
