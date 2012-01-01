2493be1f8a1f170d9014149310b3e69d677b5cfe
Marijn van der Zee <marijn@serraict.com>
2012-01-01 13:51:34 +0100

    q8492994 - method invoking factory method

---
+++ spring-questions/q8492994-exception-from-methodinvokingfactoryobject/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using NUnit.Framework;
+using Spring.Context.Support;
+
+namespace q8492994
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Main()
+        {
+            XmlApplicationContext ctx;
+            try
+            {
+                ctx = new XmlApplicationContext("objects.xml");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Assert.Pass("This exception is expected.");
+                throw;
+            }
+        }
+    }
+
+    public class MyClass
+    {
+    }
+
+    public class ExceptionThrowingClass
+    {
+        public void MethodThatThrows()
+        {
+            throw new Exception("Unexpected exception ... how to catch it?");
+        }
+    }
+}
---
+++ spring-questions/q8492994-exception-from-methodinvokingfactoryobject/question.md
@@ -0,0 +1,17 @@
+﻿I have Spring.Configuration like that:
+
+    <objects xmlns="http://www.springframework.net">
+        <!-- Create SQLite connection -->
+        <object id="SqlConnection" type="System.Data.SQLite.SQLiteConnection, System.Data.SQLite">
+            <constructor-arg index="0" value="Data Source=MyDatabase.db"/>
+        </object>
+        <!-- Open connection -->
+        <object type="Spring.Objects.Factory.Config.MethodInvokingFactoryObject, Spring.Core">
+            <property name="TargetObject">
+                <ref local="SqlConnection" />
+            </property>
+            <property name="TargetMethod" value="Open"/>
+        </object>
+    </objects>
+
+So how to handle exception from method Open() if this throws since MethodInvokingFactoryObject is create and invoke?
