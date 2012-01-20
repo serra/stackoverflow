60f733893cff6c3d8e6f81e1c15cbe23322f8ad0
Marijn van der Zee <marijn@serraict.com>
2012-01-20 10:24:08 +0100

    Add q8932396_inheritance_based_proxies

---
+++ spring-questions/q8932396_inheritance_based_proxies/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Spring.Context.Support;
+
+namespace q8932396_inheritance_based_proxies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Main()
+        {
+            var ctx = new XmlApplicationContext("objects.xml");
+            var o = ctx.GetObject("MyObject");
+            Console.WriteLine(o);
+        }
+    }
+
+    public class MyClass
+    {
+
+    }
+
+}
