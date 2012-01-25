2c83d8fb158cb5271ab72b029f3e66a803576002
Marijn van der Zee <marijn@serraict.com>
2012-01-25 13:16:04 +0100

    Simple tests with inheritance based proxies

--- spring-questions/q8932396_inheritance_based_proxies/Program.cs
+++ spring-questions/q8932396_inheritance_based_proxies/Program.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using Spring.Context.Support;
 
+
 namespace q8932396_inheritance_based_proxies
 {
+    /// <summary>
+    /// http://stackoverflow.com/questions/8932396/why-are-pure-inheritance-based-proxies-bad-in-aop
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
@@ -20,14 +24,27 @@ namespace q8932396_inheritance_based_proxies
         public void Main()
         {
             var ctx = new XmlApplicationContext("objects.xml");
-            var o = ctx.GetObject("MyObject");
+            var o = (MyClass)ctx.GetObject("MyObject");
+
+            o.Method1();
+
             Console.WriteLine(o);
         }
     }
 
     public class MyClass
     {
+        public virtual void Method1()
+        {
+            Console.WriteLine("1: This is Method1()");
 
+            Method2();
+        }
+
+        public virtual void Method2()
+        {
+            Console.WriteLine("2: This is Method2()");
+        }
     }
 
 }
