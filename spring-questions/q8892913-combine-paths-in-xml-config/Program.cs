ef571b3025431eac4ca74ff88b5ba4ee9bcae798
Marijn van der Zee <marijn@serraict.com>
2012-02-11 20:50:55 +0100

    combine paths in xml

---
+++ spring-questions/q8892913-combine-paths-in-xml-config/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using NUnit.Framework;
+using Spring.Context.Support;
+
+namespace q8892913
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = System.IO.Path.Combine(@"c:\dev", "temp");
+            Console.WriteLine(path);
+            Console.ReadLine();
+        }
+    }
+
+    [TestFixture]
+    public class DictionaryTests
+    {
+        [Test]
+        public void Main()
+        {
+            var ctx = new XmlApplicationContext("objects.xml");
+            var o = (MyClass)ctx.GetObject("MyObject");
+
+            Assert.AreEqual(@"c:\\dev\\temp", o.Path);
+
+            Console.WriteLine(o);
+        }
+    }
+
+    public class MyClass
+    {
+        public string Path { get; set; }
+    }
+
+}
