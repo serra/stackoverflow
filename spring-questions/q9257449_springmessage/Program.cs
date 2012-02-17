4a272e580de443c26a098ae96c18d7fd876b4066
Marijn van der Zee <marijn@serraict.com>
2012-02-17 09:10:18 +0100

    Add localized message injection project

---
+++ spring-questions/q9257449_springmessage/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Spring.Context.Support;
+
+namespace q9257449_springmessage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+
+    [TestFixture]
+    public class DictionaryTests
+    {
+        [Test]
+        public void Main()
+        {
+            var ctx = new XmlApplicationContext("objects.xml");
+            var o = (MyClass)ctx.GetObject("MyObject");
+            Console.WriteLine(o);
+
+            var ms = (ResourceSetMessageSource)ctx["messageSource"];
+
+            Assert.AreEqual("Hello mr. Anderson", ms.GetMessage("HelloMessage", "mr.", "Anderson"));
+
+            Assert.AreEqual("Hello mr. Anderson", o.Greeting);
+        }
+    }
+
+    public class MyClass
+    {
+        public string Greeting { get; set; }
+    }
+
+}
