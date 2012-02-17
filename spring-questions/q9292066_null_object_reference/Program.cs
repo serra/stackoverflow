ab32baee153e97f989f73aaff1a8e00f80411c3d
Marijn van der Zee <marijn@serraict.com>
2012-02-17 09:10:01 +0100

    Add null object referenc project

---
+++ spring-questions/q9292066_null_object_reference/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Spring.Context.Support;
+
+namespace q9292066_null_object_reference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+
+    [TestFixture]
+    public class DictionaryTests
+    {
+        [Test]
+        public void Main()
+        {
+            var ctx = ContextRegistry.GetContext(); //new XmlApplicationContext("objects.xml");
+            var o = ctx.GetObject("MyObject");
+            Console.WriteLine(o);
+        }
+    }
+
+    public class MyClass
+    {
+        public MyClass()
+        {
+        }
+
+        public MyClass(MyClass ref1)
+        {
+        }
+    }
+
+}
