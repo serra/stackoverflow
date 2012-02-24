a07fbd91dfb3ca618d5fe94b27a00ead591ce572
Marijn van der Zee <marijn@serraict.com>
2012-02-24 17:40:58 +0100

    Sketch for using method injection for multiple contexts

---
+++ spring-questions/q9432057_register_child_context_components/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Spring.Context.Support;
+
+namespace q9432057_register_child_context_components
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Main()
+        {
+            var ctx = ContextRegistry.GetContext("ParentContext"); // load from app.config
+
+            var ctx1 = ContextRegistry.GetContext("ChildContext1");
+            var ctx2 = ContextRegistry.GetContext("ChildContext2");
+
+            Assert.AreEqual(ctx, ctx1.ParentContext);
+            Assert.AreEqual(ctx, ctx2.ParentContext);
+
+            //Assert.AreEqual(3, ctx.GetObjectsOfType(typeof(IComponent)).Count);
+
+            Assert.IsInstanceOf<ComponentRepository>(ctx1["repo"]);
+            Assert.IsInstanceOf<ComponentRepository>(ctx2["repo"]);
+
+            var o = ctx.GetObject("MyObject");
+            Console.WriteLine(o);
+        }
+
+    }
+
+    public interface IComponent
+    {
+        string Name { get; }
+    }
+
+    public class MyClass : IComponent
+    {
+        public String Name { get; set; }
+
+        public virtual object Register()
+        {
+            return null;
+        }
+    }
+
+    public class ComponentRepository
+    {
+        public void Register(IComponent component)
+        {
+            Console.WriteLine(component.Name);
+        }
+    }
+
+}
---
+++ spring-questions/q9432057_register_child_context_components/RegistrationHelper.cs
@@ -0,0 +1,43 @@
+﻿using System.Reflection;
+using Spring.Context;
+using Spring.Objects.Factory.Support;
+
+namespace q9432057_register_child_context_components
+{
+    public class RegistrationHelper : IMethodReplacer
+    {
+        private ComponentRepository _repo;
+
+        public RegistrationHelper(ComponentRepository repo)
+        {
+        }
+
+        public object Implement(object target, MethodInfo method, object[] arguments)
+        {
+            var component = target as IComponent;
+            if (target == null)
+                return 0;
+
+            _repo.Register(component);
+            return 1;
+        }
+    }
+
+    /*
+    public class ServiceLocatorImplementer : IMethodReplacer
+    {
+        protected IEnumerable<ICustomInterfaceThatDoesSomething> GetAllImplementers()
+        {
+            var idObjects = Spring.Context.Support.ContextRegistry.GetContext()
+                .GetObjectsOfType(typeof(ICustomInterfaceThatDoesSomething));
+
+            return idObjects.Values.Cast<ICustomInterfaceThatDoesSomething>();
+        }
+
+        public object Implement(object target, MethodInfo method, object[] arguments)
+        {
+            return GetAllImplementers();
+        }
+    }
+    */
+}
