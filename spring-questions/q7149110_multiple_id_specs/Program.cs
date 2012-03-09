85211d6aeb7a109172c9cb1388a9a1277713b0b4
Marijn van der Zee <marijn@serraict.com>
2012-03-09 14:10:59 +0100

    Verify that we can use the same id more than once.

---
+++ spring-questions/q7149110_multiple_id_specs/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Spring.Context.Support;
+
+namespace q7149110_multiple_id_specs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Main()
+        {
+            var ctx = new XmlApplicationContext("objects.xml","objects2.xml");
+            var o = (MyClass)ctx.GetObject("MyObject");
+
+            Assert.AreEqual("From objects2.xml", o.Name);
+            Console.WriteLine(o);
+        }
+    }
+
+    public class MyClass
+    {
+        public string Name { get; set; }
+    }
+
+}
