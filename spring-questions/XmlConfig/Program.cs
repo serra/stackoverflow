c51565a06be3f93ae2df33e29677010dafd17f02
Marijn van der Zee <marijn@serraict.com>
2012-04-22 10:41:10 +0200

    Add project for xml config scenarios;
    answer to http://stackoverflow.com/questions/10266009/generic-dictionary-containing-generic-dictionary-in-spring-net/10266461#10266461

---
+++ spring-questions/XmlConfig/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Spring.Context.Support;
+
+namespace XmlConfig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+
+    [TestFixture]
+    public class DictionaryTests
+    {
+        [Test]
+        public void Main()
+        {
+            var ctx = new XmlApplicationContext("objects.xml");
+            
+
+            var list1 = ctx["list1"];
+            var list2 = ctx["list2"];
+            var dic1 = (Dictionary<string, List<string>>)ctx["dic1"];
+            var dic0 = (Dictionary<string, Dictionary<string, List<string>>>)ctx["dic0"];
+            var o = ctx.GetObject("MyObject");
+
+            Console.WriteLine(o);
+        }
+    }
+
+    public class MyClass
+    {
+        public Dictionary<string, Dictionary<string, List<string>>> ContextMenuModel { get; set; }
+    }
+
+}
