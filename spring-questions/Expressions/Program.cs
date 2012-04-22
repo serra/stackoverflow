9f58182032d90eb700814e529b4d08d4aabb1ac3
Marijn van der Zee <marijn@serraict.com>
2012-04-22 10:40:21 +0200

    Add project for questions related to expressions.

---
+++ spring-questions/Expressions/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Spring.Context.Support;
+
+namespace Expressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+
+    [TestFixture]
+    public class DictionaryTests
+    {
+        [Test]
+        public void Main()
+        {
+            var ctx = new XmlApplicationContext("objects.xml");
+            var o = ctx.GetObject("MyObject");
+            Console.WriteLine(o);
+        }
+    }
+
+    public class MyClass
+    {
+
+    }
+
+}
---
+++ spring-questions/Expressions/Q10150013_EvaluateProperty.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using NUnit.Framework;
+using Spring.Core.TypeResolution;
+using Spring.Expressions;
+
+namespace Expressions
+{
+    [TestFixture]
+    public class Q10150013_EvaluateProperty
+    {
+        [Test]
+        public void First_test()
+        {
+            TypeRegistry.RegisterType("IMailMessage", typeof(IMailMessage));
+
+            var mm = (IMailMessage) new MailMessage();
+            mm.Subject = "abc vijay def";
+            // does not work; spel thinks it's a MailMessage
+            //var val = ExpressionEvaluator.GetValue(mm, "(#root as T(IMailMessage)).Subject");
+            var arr = new IMailMessage[] {mm};
+            var val = ExpressionEvaluator.GetValue(arr, "(convert(IMailMessage))[0].Subject");
+            Console.WriteLine(val);
+            //Assert.True(val);
+        }
+    }
+
+    public interface IMailMessage
+    {
+        string To { get; set; }
+        string From { get; set; }
+        string Subject { get; set; }
+        string Message { get; set; }
+    }
+
+    public class MailMessage :IMailMessage
+    {
+        string IMailMessage.To { get; set; }
+        string IMailMessage.From { get; set; }
+        string IMailMessage.Subject { get; set; }
+        string IMailMessage.Message { get; set; }
+    }
+}
