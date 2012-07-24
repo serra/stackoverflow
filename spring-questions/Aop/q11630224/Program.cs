bae12648ead6102fa5c7ce689a76e0cfe7fdb7eb
Marijn van der Zee <marijn@serraict.com>
2012-07-24 17:09:18 +0200

    Some tests for http://stackoverflow.com/questions/11630224/spring-net-aop-pointcut-issue

---
+++ spring-questions/Aop/q11630224/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using AopAlliance.Intercept;
+using NUnit.Framework;
+using Spring.Context.Support;
+
+namespace Aop.q11630224
+{
+    [TestFixture]
+    public class InterceptionTests
+    {
+        [Test]
+        public void Main()
+        {
+            var ctx = new XmlApplicationContext("q11630224/objects.xml");
+            var report1 = (IReport)ctx.GetObject("report1");
+            var report2 = (IReport)ctx.GetObject("report1");
+            Console.WriteLine(UsageTrackingInterceptor.UsageCount);
+            report1.WriteReport();
+            report2.WriteReport();
+            Console.WriteLine(UsageTrackingInterceptor.UsageCount);
+        }
+    }
+
+    public class Report : IReport
+    {
+        public void WriteReport()
+        {
+            Console.WriteLine("Writing report ... Done!");
+        }
+    }
+
+    public interface IReport
+    {
+        void WriteReport();
+    }
+
+    public class UsageTrackingInterceptor : IMethodInterceptor
+    {
+        public static int UsageCount { get; private set; }
+
+        public object Invoke(IMethodInvocation invocation)
+        {
+            UsageCount++;
+            return invocation.Proceed();
+        }
+    } 
+}
