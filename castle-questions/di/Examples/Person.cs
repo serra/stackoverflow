a90aa5df25240a7177ebbfc2a09669d675c283d0
Marijn van der Zee <marijn@serraict.com>
2012-08-14 09:30:12 +0200

    Inspect life style configuration options in castle windsor

---
+++ castle-questions/di/Examples/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Di.Examples
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public Guid Id { get; private set; }
+        
+        public Person()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
---
+++ castle-questions/di/LifeStyleTests.cs
@@ -0,0 +1,72 @@
+﻿using Castle.Core;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using Di.Examples;
+using NUnit.Framework;
+
+namespace di
+{
+    // insipiration: http://stackoverflow.com/questions/11936862/windsor-transient-lifestyle
+
+    [TestFixture]
+    public class LifeStyleTests
+    {
+        [Test]
+        public void ANewPersonHasAUniqueId()
+        {
+            var p1 = new Person();
+            var p2 = new Person();
+            Assert.AreNotEqual(p1.Id, p2.Id);
+        }
+
+        [Test]
+        public void TransientPersonsHaveUniqueIds()
+        {
+            using (var c = new WindsorContainer())
+            {
+                c.Register(Component.For<Person>().ImplementedBy<Person>().LifestyleTransient());
+
+                var p1 = c.Resolve<Person>();
+                var p2 = c.Resolve<Person>();
+
+                Assert.AreNotEqual(p1.Id, p2.Id);
+            }
+        }
+
+        [Test]
+        public void SingletonPersonsHaveUniqueIds()
+        {
+            using (var c = new WindsorContainer())
+            {
+                c.Register(Component.For<Person>().ImplementedBy<Person>());
+
+                var p1 = c.Resolve<Person>();
+                var p2 = c.Resolve<Person>();
+
+                Assert.AreEqual(p1.Id, p2.Id);
+            }
+        }
+
+        [Test]
+        public void HandlingComponentModelCreatedCanSetLifestyle()
+        {
+            using (var c = new WindsorContainer())
+            {
+                c.Kernel.ComponentModelCreated += SetUndefinedLifeStyleToTransient;
+                c.Register(Component.For<Person>().ImplementedBy<Person>());
+
+                var p1 = c.Resolve<Person>();
+                var p2 = c.Resolve<Person>();
+
+                Assert.AreNotEqual(p1.Id, p2.Id);
+            }
+        }
+
+        void SetUndefinedLifeStyleToTransient(Castle.Core.ComponentModel model)
+        {
+            if (model.LifestyleType == LifestyleType.Undefined)
+                model.LifestyleType = LifestyleType.Transient;
+
+        }
+    }
+}
