1ee1e36f2bbe8e5c516d612cadfe1835c7b19a87
Marijn van der Zee <marijn@serraict.com>
2012-08-29 09:59:24 +0200

    Use `VariablePlaceholderConfigurer` for string constants
    Use `VariablePlaceholderConfigurer` for string constants as an
    alternative to configuring strings in the container.

---
+++ spring-questions/XmlConfig/StringInjection/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace XmlConfig.StringInjection
+{
+    public class Person
+    {
+        public string Name { get; set; }
+ 
+    }
+}
---
+++ spring-questions/XmlConfig/StringInjection/StringInjection.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using Spring.Context.Support;
+using Spring.Objects.Factory.Config;
+
+namespace XmlConfig.StringInjection
+{
+    [TestFixture]
+    public class StringInjection
+    {
+        [Test]
+        public void InjectStringFromVariableSource()
+        {
+            var ctx = new XmlApplicationContext("StringInjection/objects.xml");
+
+            var s = (string) ctx["nameFromStringObjectDefinition"];
+            var p1 = (Person)ctx["JohnUsingString.CopyAsFactoryMethod"];
+            Assert.AreEqual("John Doe (from string singleton)", s);
+            Assert.AreEqual("John Doe (from string singleton)", p1.Name);
+
+            var p2 = (Person)ctx["JohnUsingVariableSource"];
+            Assert.AreEqual("John Doe (from string variable source)", p2.Name);
+
+        } 
+       
+    }
+}
