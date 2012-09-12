d5b055d60db16afb3f0f7667814c50a7ad8d4aff
Marijn van der Zee <marijn@serraict.com>
2012-09-12 09:18:28 +0200

    Add custom session factory example
    http://stackoverflow.com/questions/12366414/is-there-any-way-to-exclude-some-hbm-mappings-files-from-session-factory-object

---
+++ spring-questions/NHibernateAndSpring/LoadMappingsFromFolderInAssemblySessionFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using NHibernate.Cfg;
+using Spring.Data.NHibernate;
+
+namespace NHibernateAndSpring
+{
+    public class LoadMappingsFromFolderInAssemblySessionFactory : LocalSessionFactoryObject
+    {
+        public string MyMappingAssembly { get; set; }
+        public string MyFolder { get; set; }
+
+        protected override void PostProcessConfiguration(Configuration config)
+        {
+            base.PostProcessConfiguration(config);
+            // guard methods omitted
+            Assembly asm = GetAssembly(MyMappingAssembly);
+            IEnumerable<string> paths = GetResourcePaths(MyFolder, asm);
+            config.AddResources(paths, asm);
+        }
+
+        private IEnumerable<string> GetResourcePaths(string myFolder, Assembly asm)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Assembly GetAssembly(string myMappingAssembly)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ spring-questions/NHibernateAndSpring/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Spring.Context.Support;
+
+namespace NHibernateAndSpring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Main()
+        {
+            var ctx = new XmlApplicationContext("objects.xml");
+            var o = ctx.GetObject("MyObject");
+            Console.WriteLine(o);
+        }
+    }
+
+    public class MyClass
+    {
+
+    }
+
+}
