75c75a137bc7c91b5915742104b844c4ca95f80c
Marijn van der Zee <marijn@serraict.com>
2013-11-12 09:52:57 +0100

    Add working example for this question

---
+++ spring-questions/context_hierarchy_q19858418/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Spring.Context.Support;
+
+namespace context_hierarchy_q19858418
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ctx = ContextRegistry.GetContext("Parent");
+            var o1 = (MyClass)ctx.GetObject("MyObject");
+            var o3 = (MyClass)ctx.GetObject("MyObject3");
+            
+            var ctx2 = ContextRegistry.GetContext("child");
+            var o2 = (MyClass)ctx2.GetObject("MyObject2");
+            Console.WriteLine("{0} name: '{1}'", o1, o1.Name);
+            Console.WriteLine("{0} name: '{1}'", o3, o3.Name);
+            Console.WriteLine("{0} name: '{1}'", o2, o2.Name);
+            Console.ReadLine();
+        }
+    }
+
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Main()
+        {
+            var ctx = ContextRegistry.GetContext();
+            var o = ctx.GetObject("MyObject");
+            Console.WriteLine(o);
+        }
+    }
+
+    public class MyClass
+    {
+        public string Name { get; set; }
+    }
+
+}
